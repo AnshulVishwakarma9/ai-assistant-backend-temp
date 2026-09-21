51cbbd216d8e803e15d9a0653665a9c5fe766cce
Anshul Vishwkarma <anshul.vishwakarma1357@gmail.com>
2026-09-21 19:54:21 +0530

    Update CORS middleware to include new origin
    Added a new allowed origin for CORS in FastAPI middleware.

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+                  "http://localhost:5173", 
+                  "http://127.0.0.1:5173",
+                  "https://ai-chat-app-av-dev.vercel.app"
+                    ],
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
 )
